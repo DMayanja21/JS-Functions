cdd231e9672e5482bfd116092df9bb77e250bbc4
DMayanja21 <you@example.com>
2019-06-18 12:33:19 +0300

    edited

--- main.js
+++ main.js
@@ -1,24 +1,26 @@
-function numberGame(){
-	var randomNumber= Math.floor(Math.random()*100)+1;
-	var y=randomNumber%2;
-	if (y==0)
-		alert("The number is Even");
-	else
-		alert("The number is Odd");
-	for (count=1;count<=10;count++){
-		let num=prompt("Please enter a number:");
-		if (num>randomNumber){
-			alert("Your number is higher. Make another guess");
-		}
-		if (num<randomNumber){
-			alert("Your number is lower. Make another guess");
-		}
-		if (num==randomNumber){
-			alert("YOU WIN");
+const numberGame=(range,rounds)=>{
+var randomNumber= Math.floor(Math.random()*range)+1;
+var y=randomNumber%2;
+if (y==0)
+	alert("The number is Even");
+else
+	alert("The number is Odd");
+for (count=1;count<=rounds;count++)
+{
+	let num=prompt("Please enter a number:");
+	if (num>randomNumber){
+		alert("Your number is higher. Make another guess");
+	}
+	if (num<randomNumber){
+		alert("Your number is lower. Make another guess");
+	}
+	if (num==randomNumber){
+		alert("YOU WIN");
 		break;
 	}
-		if (count==10) {
-			alert("YOU LOSE");
-		}
+	if (count==rounds) {
+		alert("YOU LOSE");
+	}
 }
 }
+numberGame(100,10);
